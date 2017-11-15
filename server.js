411fe07b001ba9b71ac234db79dd65d54079b966
Jonatan Michalak <michalakjonatan@gmail.com>
2017-11-15 13:05:26 +0100

    Basic server with vue

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs   = require('fs');
+
+const server = http.createServer((req,res) => {
+    if (req.method == 'GET') {
+	if(req.url == '/') {
+	    var data = fs.readFileSync('./index.html');
+	    res.write(data);
+	    res.end();
+	}
+	else if (req.url == '/app.js') {
+	    var data = fs.readFileSync('./app.js');
+	    res.write(data);
+	    res.end();
+	}
+	else if (req.url == '/node_modules/vue/dist/vue.js') {
+	    var data = fs.readFileSync('./node_modules/vue/dist/vue.js');
+	    res.write(data);
+	    res.end();
+	}
+    }
+});
+
+server.listen(8080);
