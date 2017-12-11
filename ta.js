53cf2fcefca7780bede6f8d5cec4e99ff17ec5f4
Jonatan Michalak <michalakjonatan@gmail.com>
2017-12-12 00:30:27 +0100

    ta can only help one student

--- ta.js
+++ ta.js
@@ -3,26 +3,26 @@ const socket = io();
 socket.emit('getStudentList');
 
 socket.on('needHelpList', function(data) {
-    console.log("'needHelpList' recieved from server");
     app.needHelpList = data.needHelpList;
     app.helpedList = data.helpedList;
-    console.log(app.needHelpList);
-    console.log(app.helpedList);
 });
 
 var app = new Vue({
     el: '#app',
     data: {
         needHelpList: [],
-        helpedList: []
+        helpedList: [],
+	isHelping: false,
+	studentHelpingName: ""
     },
     methods: {
         emit: (data) => {
             socket.emit('studentPickedUp', data);
         },
-        pickup: (id) => {
-            console.log(id);
-            socket.emit('pickup', id);
+        pickup: (person) => {
+            socket.emit('pickup', person.id);
+	    app.isHelping = true;
+	    app.studentHelpingName = person.name;
         }
     }
 });
